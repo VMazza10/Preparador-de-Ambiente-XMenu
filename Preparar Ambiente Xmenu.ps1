d51793eca9f2102d01fe88ed430419c7dc26a753
VMazza10 <vinim00@hotmail.com>
2026-08-27 00:59:58 -0300

    67

--- Preparar Ambiente Xmenu.ps1
+++ Preparar Ambiente Xmenu.ps1
@@ -4275,6 +4275,7 @@ function Open-Selector {
     
     $versions = @()
     if ($Type -eq "PDV") {
+        $versions += @{Name = "NetPDV v1.3.67.0"; Url = "https://netcontroll.com.br/util/instaladores/netpdv/1.3/67/0/NetPDV.zip"; File = "NetPDV_1.3.67.0.zip" }
         $versions += @{Name = "NetPDV v1.3.64.0"; Url = "https://netcontroll.com.br/util/instaladores/netpdv/1.3/64/0/NetPDV.zip"; File = "NetPDV_1.3.64.0.zip" }
         $versions += @{Name = "NetPDV v1.3.63.0"; Url = "https://netcontroll.com.br/util/instaladores/netpdv/1.3/63/0/NetPDV.zip"; File = "NetPDV_1.3.63.0.zip" }
         $versions += @{Name = "NetPDV v1.3.60.0"; Url = "https://netcontroll.com.br/util/instaladores/netpdv/1.3/60/0/NetPDV.zip"; File = "NetPDV_1.3.60.0.zip" }
